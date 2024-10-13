429c828f3241bd6499c97073055b0f9de767e970
lycblank <longyongchun@yuanpinda.com>
2024-10-13 19:04:29 +0800

    [ADD] 第一次初始化

---
+++ bash/watch.c
@@ -0,0 +1,32 @@
+//go:build ignore
+
+#include <vmlinux.h>
+#include <bpf/bpf_helpers.h>
+#include <bpf/bpf_tracing.h>
+
+#define MAX_LINE_SIZE 500
+
+struct bash_info {
+    char content[MAX_LINE_SIZE];
+    u32 pid;
+};
+
+struct {
+    __uint(type, BPF_MAP_TYPE_QUEUE); 
+    __type(value, struct bash_info);
+    __uint(max_entries, 64);
+} bash_input SEC(".maps");
+
+SEC("uretprobe//bin/bash:readline")
+int BPF_KRETPROBE(watch_bash, const void *ret) {
+    struct bash_info info;
+    info.pid = bpf_get_current_pid_tgid() >> 32;
+    bpf_probe_read_user_str(info.content, sizeof(info.content), ret);
+    bpf_map_push_elem(&bash_input, &info, BPF_EXIST);
+
+    bpf_printk("PID %d read: %s ", info.pid, info.content);
+
+    return 0;
+};
+
+char LICENSE[] SEC("license") = "GPL";
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/lycblank/bpf
+
+go 1.23.2
