7bec9e8c26f5a9dd3240a2b9dcf36bbe85123fae
lycblank <longyongchun@yuanpinda.com>
2024-10-27 19:20:16 +0800

    [ADD] 增加go程序读取map

--- bash/watch.go
+++ bash/watch.go
@@ -1,5 +1,45 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"log"
+	"time"
 
+	"github.com/cilium/ebpf/link"
+	"github.com/cilium/ebpf/rlimit"
+)
+
+func main() {
+	// Remove resource limits for kernels <5.11.
+	if err := rlimit.RemoveMemlock(); err != nil {
+		log.Fatal("Removing memlock:", err)
+	}
+
+	var objs watchObjects
+	if err := loadWatchObjects(&objs, nil); err != nil {
+		log.Fatal("Loading eBPF objects:", err)
+	}
+	defer objs.Close()
+
+	ex, err := link.OpenExecutable(`/bin/bash`)
+	if err != nil {
+		log.Fatal("open excutable:", err)
+	}
+
+	up, err := ex.Uretprobe(`readline`, objs.WatchBash, nil)
+	if err != nil {
+		log.Fatal("Uretprobe:", err)
+	}
+	defer up.Close()
+
+	bashInfo := watchBashInfo{}
+	ticker := time.NewTicker(1 * time.Second)
+	defer ticker.Stop()
+
+	for range ticker.C {
+		if err := objs.BashInput.LookupAndDelete(nil, &bashInfo); err != nil {
+			fmt.Printf("Loading eBPF objects: %+v\n", err)
+		}
+		fmt.Printf("get pid: %d\n", bashInfo.Pid)
+	}
 }
